0289bbe55ca306ef33a136438709e71cea42ee9b
Nathalia Gutierrez <nathaliagpinilla@gmail.com>
2026-07-25 05:43:29 -0500

    Ejercicio1

---
+++ Funciones/Ejercicio1.py
@@ -0,0 +1,17 @@
+## Crear un programa que tenga una lista, luego crear una funcion con la cual
+## se van a pedir al usuario numeros para agresar a la lista. 
+## Se debe crear una funcion en donde se ordenen los numeros pares e impares dentro de las listas.
+
+lista = []
+num = 0 
+
+def pedir():
+    i = 0 
+    while i <= 5:
+        num = float(input("Ingresa un numero: "))
+        lista.append(num)
+        i += 1
+       
+pedir()
+print(lista)
+ 
