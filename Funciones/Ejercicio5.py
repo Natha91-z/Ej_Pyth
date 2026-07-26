1211316f0ef7354849ce080a05cff3fb117cd833
Nathalia Gutierrez <nathaliagpinilla@gmail.com>
2026-07-26 05:22:50 -0500

    Ejercicio altura cuadrado y circulo

---
+++ Funciones/Ejercicio5.py
@@ -0,0 +1,13 @@
+# Crear dos dunciones una para tener el area de un cuadrado y la otra de un Circulo
+
+def areaCuadrado(base, altura):
+    area = base * altura
+    return (area)
+
+areaCuadrado(10, 10)
+
+def areaCirculo(radio):
+    return pow(radio, 2) * 3.14
+
+print(areaCirculo(10))
+
