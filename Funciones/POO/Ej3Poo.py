0fe4b36e449443f95c8818990fc576e961400141
Nathalia Gutierrez <nathaliagpinilla@gmail.com>
2026-07-31 03:45:21 -0500

    update Ej3Poo.py

---
+++ Funciones/POO/Ej3Poo.py
@@ -0,0 +1,26 @@
+#Crear una clase Fabrica que tenga los atributos de Llantas, Color y Precio; luego crear dos clases mas que hereden de Fabrica, las cuales son Moto y Carro. Crear metodos que muestren la cantidad de llantas, color y precio de ambos transportes. Por ultimo, crear objetos para cada clase y mostrar por pantalla los atributos de cada uno
+
+
+class Fabrica():
+    def __init__(self, llantas, color, precio):
+        self.llantas = llantas
+        self.color = color
+        self.precio = precio
+        
+class Carro(Fabrica):
+    def datos(self):
+        print("La cantidad de llantas es de: ", self.llantas)
+        print(" El color del carro es: ", self.color)
+        print(" El precio del carro es: ", self.precio)
+        
+class Moto(Fabrica):
+    def datos(self):
+        print("La cantidad de llantas es de; ", self.llantas)
+        print("El color de la moto es: ", self.color)
+        print("El precio de la moto es: ", self.precio)
+        
+moto = Moto(2, "Negro", 10.000)
+moto.datos()
+
+carro = Carro(4, "Blanco", 23.000)
+carro.datos()
