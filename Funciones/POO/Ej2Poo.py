52f4c84db2f15fa8c99d0f131a0579d6e2acf755
Nathalia Gutierrez <nathaliagpinilla@gmail.com>
2026-07-31 03:06:44 -0500

    Ejercicio 2

---
+++ Funciones/POO/Ej2Poo.py
@@ -0,0 +1,4 @@
+# Realizar un programa el cual se declaren dos valores enteros utlizando
+# el metodo __init__. Calcular la +, -,*. /. Usar el metodo de cada una y 
+# llamar a la clase calculadora. Mostrar resultados. 
+
